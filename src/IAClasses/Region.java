86e6251e1a02604c8aa2448634e49bfa1c72e424
David J Barry <davejpbarry@gmail.com>
2014-09-02 11:16:36 +0100

    Region.getPolygonRoi() modified to fill holes in mask prior to generating boundary

--- src/IAClasses/Region.java
+++ src/IAClasses/Region.java
@@ -1,5 +1,6 @@
 package IAClasses;
 
+import ij.IJ;
 import ij.ImagePlus;
 import ij.ImageStack;
 import ij.gui.PolygonRoi;
@@ -241,13 +242,55 @@ public class Region {
             tempImage.drawPixel(borderPix.get(i).getX(), borderPix.get(i).getY());
 //            poly.addPoint(borderPix.get(i).getX(), borderPix.get(i).getY());
         }
+        fill(tempImage, FOREGROUND, BACKGROUND);
+        IJ.saveAs((new ImagePlus("", tempImage)), "PNG", "C:/users/barry05/desktop/tempImage.png");
         Wand wand = new Wand(tempImage);
         wand.autoOutline(borderPix.get(0).getX(), borderPix.get(0).getY(), FOREGROUND, FOREGROUND);
-        int xpix[] =new int[wand.npoints];
-        int ypix[] =new int[wand.npoints];
+        int xpix[] = new int[wand.npoints];
+        int ypix[] = new int[wand.npoints];
         System.arraycopy(wand.xpoints, 0, xpix, 0, wand.npoints);
         System.arraycopy(wand.ypoints, 0, ypix, 0, wand.npoints);
         return new PolygonRoi(xpix, ypix, wand.npoints, Roi.POLYGON);
+    }
+
+    /**
+     * Binary fill by Gabriel Landini, G.Landini at bham.ac.uk. Copied from
+     * ij.plugin.filter.Binary
+     *
+     * @param ip
+     * @param foreground
+     * @param background
+     */
+    void fill(ImageProcessor ip, int foreground, int background) {
+        int width = ip.getWidth();
+        int height = ip.getHeight();
+        FloodFiller ff = new FloodFiller(ip);
+        ip.setColor(127);
+        for (int y = 0; y < height; y++) {
+            if (ip.getPixel(0, y) == background) {
+                ff.fill(0, y);
+            }
+            if (ip.getPixel(width - 1, y) == background) {
+                ff.fill(width - 1, y);
+            }
+        }
+        for (int x = 0; x < width; x++) {
+            if (ip.getPixel(x, 0) == background) {
+                ff.fill(x, 0);
+            }
+            if (ip.getPixel(x, height - 1) == background) {
+                ff.fill(x, height - 1);
+            }
+        }
+        byte[] pixels = (byte[]) ip.getPixels();
+        int n = width * height;
+        for (int i = 0; i < n; i++) {
+            if (pixels[i] == 127) {
+                pixels[i] = (byte) background;
+            } else {
+                pixels[i] = (byte) foreground;
+            }
+        }
     }
 
     public double getMean() {
