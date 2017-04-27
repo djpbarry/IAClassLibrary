95f24c83238a14ecfb24164f9f2c025a7e991ea6
davejpbarry <davejpbarry@gmail.com>
2017-04-27 15:58:51 +0100

    Added new IJUtils and DataWriter classes

---
+++ src/IO/DataWriter.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2017 Dave Barry <david.barry at crick.ac.uk>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package IO;
+
+import UtilClasses.GenVariables;
+import ij.text.TextWindow;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+
+public class DataWriter {
+
+    /**
+     *
+     * @param tw
+     * @throws IOException
+     */
+    public static void saveTextWindow(TextWindow tw, File dataFile, String headings) throws IOException {
+        CSVPrinter printer = new CSVPrinter(new OutputStreamWriter(new FileOutputStream(dataFile), GenVariables.ISO), CSVFormat.EXCEL);
+        int L = tw.getTextPanel().getLineCount();
+        printer.printRecord(headings);
+        for (int l = 0; l < L; l++) {
+            printer.printRecord(tw.getTextPanel().getLine(l).replace("\t", ","));
+        }
+        printer.close();
+    }
+
+    public static void saveValues(double[][] vals, File dataFile) throws IOException {
+        CSVPrinter printer = new CSVPrinter(new OutputStreamWriter(new FileOutputStream(dataFile), GenVariables.ISO), CSVFormat.EXCEL);
+        int L = vals.length;
+        for (int l = 0; l < L; l++) {
+            for (double v : vals[l]) {
+                printer.print(v);
+            }
+            printer.println();
+        }
+        printer.close();
+    }
+    
+}
