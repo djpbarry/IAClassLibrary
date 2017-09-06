96381d3f9af06072303ed4a340118f56c7a2864b
davejpbarry <davejpbarry@gmail.com>
2017-09-06 15:34:29 +0100

    Added Blob, Point and ParticleWriter classes

---
+++ src/ParticleWriter/ParticleWriter.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2017 Dave Barry <david.barry at crick.ac.uk>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package ParticleWriter;
+
+import IAClasses.Utils;
+import Particle.Blob;
+import Particle.IsoGaussian;
+import Particle.Particle;
+import ij.process.ImageProcessor;
+import java.awt.Color;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Dave Barry <david.barry at crick.ac.uk>
+ */
+public class ParticleWriter {
+
+    public static void drawParticle(ImageProcessor image,
+            Particle p, boolean preview, double blobSize, double spatialRes) {
+        int radius = (int) Math.round(blobSize / spatialRes);
+        int x = (int) Math.round(p.getX() / spatialRes);
+        int y = (int) Math.round(p.getY() / spatialRes);
+        if (p instanceof Blob) {
+            image.drawOval((x - radius), (y - radius), 2 * radius, 2 * radius);
+        } else if (p instanceof IsoGaussian) {
+            if (preview) {
+                radius = (int) Math.round(((IsoGaussian) p).getXSigma() / spatialRes);
+                image.drawOval((x - radius), (y - radius), 2 * radius, 2 * radius);
+            } else {
+                Utils.draw2DGaussian(image, (IsoGaussian) p, 0.0, spatialRes, false);
+            }
+        } else {
+            image.drawLine(x, y - radius, x, y + radius);
+            image.drawLine(x + radius, y, x - radius, y);
+        }
+    }
+
+    public static void drawDetections(ArrayList<Particle> detections, ImageProcessor output, boolean preview, double blobSize, double spatialRes) {
+        for (Particle p : detections) {
+            drawParticle(output, p, preview, blobSize, spatialRes);
+        }
+    }
+
+}
