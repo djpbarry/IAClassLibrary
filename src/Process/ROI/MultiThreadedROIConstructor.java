19a4a3fe8f332524a004a33c49d65961c0781769
Dave Barry <davejpbarry@gmail.com>
2018-11-13 16:34:37 +0000

    Shifted functionality from Segmentation Panel into MultiThreadedROIConstructor

--- src/Process/ROI/MultiThreadedROIConstructor.java
+++ src/Process/ROI/MultiThreadedROIConstructor.java
@@ -20,8 +20,11 @@ import IO.BioFormats.BioFormatsImg;
 import Process.MultiThreadedProcess;
 import ij.ImagePlus;
 import ij.gui.Roi;
+import ij.measure.ResultsTable;
+import ij.plugin.filter.Analyzer;
 import java.util.ArrayList;
 import java.util.Properties;
+import java.util.concurrent.Executors;
 import mcib3d.geom.Object3D;
 import mcib3d.geom.Objects3DPopulation;
 import mcib3d.image3d.ImageInt;
@@ -44,20 +47,32 @@ public class MultiThreadedROIConstructor extends MultiThreadedProcess {
         this.img = img;
         this.props = props;
         this.propLabels = propLabels;
+        this.exec = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
     }
 
     @Override
     public void run() {
         ImagePlus labels = img.getProcessedImage();
         objectPop = new Objects3DPopulation(ImageInt.wrap(labels), 1);
-        Object3D[] objects = objectPop.getObjectsArray();
+        ArrayList<Object3D> objects = objectPop.getObjectsList();
         int[] dims = new int[]{labels.getWidth(), labels.getHeight(), labels.getNSlices()};
-        for (int i = 0; i < objects.length; i++) {
-            Object3D object = objects[i];
+        for (int i = 0; i < objects.size(); i++) {
+            Object3D object = objects.get(i);
             allRois.add(new ArrayList());
             exec.submit(new RunnableRoiConstructor(allRois, object, dims, i));
         }
         terminate("Error encountered building ROIs.");
+        ArrayList<double[]> measures = objectPop.getMeasuresStats(img.getLoadedImage().getImageStack());
+        ResultsTable rt = Analyzer.getResultsTable();
+        String[] headings = {"Index", "Mean Pixel Value", "Pixel Standard Deviation", "Min Pixel Value", "Max Pixel Value", "Integrated Density"};
+        for (double[] m : measures) {
+            rt.incrementCounter();
+            for (int i = 0; i < m.length; i++) {
+                rt.addValue(headings[i], m[i]);
+            }
+        }
+        rt.updateResults();
+        rt.show("Measures");
     }
 
     public ArrayList<ArrayList<Roi>> getAllRois() {
